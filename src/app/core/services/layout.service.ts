3dc47de1bcf4694e30f94abf7d878536f0c56a35
ErickSalgado <stes1999inm1bh@gmail.com>
2026-04-01 13:20:21 -0500

    feat: implement responsive sidebar layout with toggle functionality and environment configuration

--- src/app/core/services/layout.service.ts
+++ src/app/core/services/layout.service.ts
@@ -4,11 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LayoutService {
-  isCollapsed = false;
+  isCollapsed = true;
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
-    // Esto te demostrará que el clic sí llega hasta aquí
-    console.log('¿El menú está colapsado?:', this.isCollapsed); 
+  }
+
+  closeOnMobile() {
+    if (window.innerWidth < 1024) {
+      this.isCollapsed = true;
+    }
   }
 }
